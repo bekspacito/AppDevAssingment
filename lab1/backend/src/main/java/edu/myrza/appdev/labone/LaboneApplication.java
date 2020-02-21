431db5b97b103c4d4ddbedd0b8a80d4db53e49a9
myrza <Gapusta97@gmail.com>
2020-02-21 16:40:40 +0600

    [LAB 1] at the moment allowed all the origins to communicate with the backend

--- lab1/backend/src/main/java/edu/myrza/appdev/labone/LaboneApplication.java
+++ lab1/backend/src/main/java/edu/myrza/appdev/labone/LaboneApplication.java
@@ -3,12 +3,33 @@ package edu.myrza.appdev.labone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Bean;
+
+
 @SpringBootApplication
 public class LaboneApplication {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(LaboneApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfig(){
+		System.out.println("cors .... cors");
+		return new WebMvcConfigurer(){
+			
+			public void addCorsMappings(CorsRegistry registry){
+			registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "DELETE")
+				.allowedHeaders("*")
+                		.allowCredentials(true);
+			}
+
+		};
 	}
 
 }
